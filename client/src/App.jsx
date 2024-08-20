b68c014e4719d53c3a96310af2a1f3500e2b026e
dipesh <dipeshmahatto@gmail.com>
2024-08-20 13:48:44 +0545

    bootstrap install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-  // const Styling = {
-  //   color :"green",
-  //   font:"4rem"
-  // }
   return (
     <>
-      {/* <h1 style={{color:"green"}}>Hello World</h1> */}
-      {/* <h1 style={Styling}>Hello World</h1> */}
-      {/* <h1 className="hello">Hello World</h1> */}
-      <h1 className="text-red-500 text-center text-2xl">Hello World</h1>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
     </>
   );
 }
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return <h1 className="font-poppins">This is My Header</h1>
+};
+
+export default Header;
+
+
