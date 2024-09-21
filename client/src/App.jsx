b9722c087582201efb6b86c86f1ebd93a1fe9a1d
dipesh <dipeshmahatto@gmail.com>
2024-09-21 22:53:17 +0545

    cart details page modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,8 +37,8 @@ function App() {
   }, []);
   const mockCartItems = [
     { id: 1, quantity: 2 },
-    { id: 6, quantity: 1 },
-    { id: 3, quantity: 3 },
+    { id: 7, quantity: 7 },
+    { id: 5, quantity: 4 },
   ];
   return (
     <>
